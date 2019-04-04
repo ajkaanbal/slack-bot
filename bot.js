6ee8f83f9e36c0acdbceba2a3a6f1de97bee059f
Ricardo M. Vilchis <ajkaanbal@gmail.com>
2019-04-04 01:23:32 -0600

    Read the message to send from a file

--- bot.js
+++ bot.js
@@ -1,9 +1,16 @@
+const fs = require('fs')
 const { WebClient } = require('@slack/client')
 
 const token = process.env.SLACK_BOT_TOKEN
 const bot = new WebClient(token)
 
 if(require.main == module){
+  const args = process.argv
+  const channelId = args[2]
+  const filePath = args[3]
+  const file = fs.readFileSync(filePath, "utf8")
+  const message = JSON.parse(file)
+
   postMessage(channelId, message)
     .then(console.log)
     .catch(console.log)
@@ -11,7 +18,7 @@ if(require.main == module){
 
 async function postMessage(channelId, message) {
   // See: https://api.slack.com/methods/chat.postMessage
-  const res = await bot.chat.postMessage({ channel: channelId, text: message });
-  // `res` contains information about the posted message
-  console.log('Message sent: ', res.ts);
+  const res = await bot.chat.postMessage({ channel: channelId, blocks: message });
+  // const res = 1
+  return res
 }
